71b9b93d2a3f93151549e76f1f340f6980f6f068
Katie <adlerk8@gmail.com>
2020-10-16 14:11:15 -0700

    ready for review, for real this time

--- javascript-4/student.js
+++ javascript-4/student.js
@@ -5,7 +5,7 @@
 // Create a new array called 'faveColors' and assign it a value of your three favorite colors as strings.
 
 // CODE HERE
-let faveColors = ["teal", "purple", "mustard yellow"]
+let faveColors = ["#05668d", "#540d6e", "#ffb703"]
 //////////////////Step 2////////////////////
 // Create an object called 'me' that has these keys: firstName, superHeroName, homeTown, superPowers, superPowerXP, and profileImage. 
 
